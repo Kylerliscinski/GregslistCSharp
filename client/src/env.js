99a7542d1f1be678200e6406d18a607344a44847
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-14 14:23:22 -0600

    Init

--- client/src/env.js
+++ client/src/env.js
@@ -5,6 +5,6 @@ export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 
 // TODO change these variables out to your own auth after cloning!
-export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
-export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
-export const audience = 'https://jeremyisaraddude.com'
+export const domain = 'dev-igepbtkmc1ba5lje.us.auth0.com'
+export const clientId = 'aeNaB4HPylNWypTUZCNFcKeYzOVpjfx3'
+export const audience = 'https://houses.com'
