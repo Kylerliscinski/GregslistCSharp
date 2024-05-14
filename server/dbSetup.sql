4ff64ae5cf3501d9bf8d20ab26d3004616600e12
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-05-14 15:56:03 -0600

    dbSetup complete

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -69,6 +69,41 @@ SELECT * FROM cars;
 
 SELECT * FROM accounts WHERE id = "65f87bc1e02f1ee243874743";
 
-SELECT *
-FROM cars
-    JOIN accounts ON cars.creatorId = accounts.id;
+SELECT * FROM cars JOIN accounts ON cars.creatorId = accounts.id;
+
+CREATE TABLE houses (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    bedrooms VARCHAR(255) NOT NULL,
+    bathrooms VARCHAR(255) NOT NULL,
+    levels INT UNSIGNED NOT NULL,
+    price INT UNSIGNED NOT NULL,
+    imgUrl VARCHAR(1000) NOT NULL,
+    description VARCHAR(1000),
+    year INT UNSIGNED NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+);
+
+INSERT INTO
+    houses (
+        bedrooms,
+        bathrooms,
+        levels,
+        price,
+        imgUrl,
+        description,
+        year,
+        creatorId
+    )
+VALUES (
+        5,
+        3,
+        3,
+        100000,
+        "https://www.bhg.com/thmb/FcKK-L23QiqiDVjrjLgfa1uFZU8=/4000x0/filters:no_upscale():strip_icc()/101495134_preview-b192d3b7d4b04188a014754b9ffa6f79.jpg",
+        "Cool house",
+        2007,
+        "662ac3c1d33b7e95087433ae"
+    );
